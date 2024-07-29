b313ccd44bcaf7e87d699dd923e5e4d2c3c43e31
cemilesenturk <ahmet.senturk@heapgames.com>
2024-07-29 19:07:08 +0300

    Ground snapped.

--- Assets/Heap Lab/Subjects/For Beginners/2-Inputs/1-Mouse/Scripts/MouseClickHandler.cs
+++ Assets/Heap Lab/Subjects/For Beginners/2-Inputs/1-Mouse/Scripts/MouseClickHandler.cs
@@ -1,21 +1,26 @@
+﻿#define OverlayCamera
+
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+#if OverlayCamera
 public class MouseClickHandler : MonoBehaviour
 {
     public bool ShowWorldPosition;
     public bool SnapWorldPositionToGround;
     public TextMeshProUGUI PositionText;
-    public UnityEngine.Plane Plane;
+    public GameObject Ground;
     public Canvas Canvas;
 
     Camera mainCamera;
+    UnityEngine.Plane plane;
 
     private void Awake()
     {
         mainCamera = Camera.main;
+        plane = new Plane(Ground.transform.up, Ground.transform.position);
     }
     private void Update()
     {
@@ -25,7 +30,7 @@ public class MouseClickHandler : MonoBehaviour
         {
             Ray ray = mainCamera.ScreenPointToRay(clickPosition);
             float enter;
-            if (Plane.Raycast(ray, out enter))
+            if (plane.Raycast(ray, out enter))
             {
                 Vector3 planePosition = ray.GetPoint(enter);
                 PositionText.text = "Position on plane: " + planePosition.ToString();
@@ -34,9 +39,7 @@ public class MouseClickHandler : MonoBehaviour
 
                 if (Input.GetMouseButtonDown(0))
                 {
-                    RectTransform pointer = Instantiate(PositionText.rectTransform.parent.GetComponent<RectTransform>());
-                    pointer.parent = Canvas.transform;
-                    pointer.position = screenPoint;
+                    SpawnPositionText(screenPoint);
                 }
             }
         }
@@ -46,23 +49,94 @@ public class MouseClickHandler : MonoBehaviour
             PositionText.text = "World Position: " + worldPosition.ToString();
             Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(mainCamera, worldPosition);
             PositionText.rectTransform.parent.position = screenPoint;
+
             if (Input.GetMouseButtonDown(0))
             {
-                RectTransform pointer = Instantiate(PositionText.rectTransform.parent.GetComponent<RectTransform>());
-                pointer.parent = Canvas.transform;
-                pointer.position = screenPoint;
+                SpawnPositionText(screenPoint);
             }
         }
         else
         {
             PositionText.text = "Screen Position: " + clickPosition.ToString();
             PositionText.rectTransform.parent.position = clickPosition;
+
             if (Input.GetMouseButtonDown(0))
             {
-                RectTransform pointer = Instantiate(PositionText.rectTransform.parent.GetComponent<RectTransform>());
-                pointer.parent = Canvas.transform;
-                pointer.position = clickPosition;
+                SpawnPositionText(clickPosition);
             }
         }
     }
+
+    private void SpawnPositionText(Vector3 position)
+    {
+        RectTransform newText = Instantiate(PositionText.rectTransform, Canvas.transform);
+        newText.position = position;
+        newText.GetComponent<TextMeshProUGUI>().text = PositionText.text;
+    }
 }
+#else
+
+public class MouseClickHandler : MonoBehaviour
+{
+    public bool ShowWorldPosition;
+    public bool SnapWorldPositionToGround;
+    public TextMeshProUGUI PositionText;
+    public GameObject Ground;
+    public Canvas Canvas;
+
+    private Camera mainCamera;
+    private UnityEngine.Plane plane;
+
+    private void Awake()
+    {
+        mainCamera = Canvas.worldCamera; // Canvas'ın bağlı olduğu kamerayı kullanıyoruz
+        plane = new UnityEngine.Plane(Ground.transform.up, Ground.transform.position);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = Input.mousePosition;
+
+            if (SnapWorldPositionToGround && Ground != null)
+            {
+                Ray ray = mainCamera.ScreenPointToRay(clickPosition);
+                float enter;
+                if (plane.Raycast(ray, out enter))
+                {
+                    Vector3 planePosition = ray.GetPoint(enter);
+                    PositionText.text = "Position on plane: " + planePosition.ToString();
+                    Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(mainCamera, planePosition);
+                    PositionText.rectTransform.position = screenPoint;
+
+                    SpawnPositionText(screenPoint);
+                }
+            }
+            else if (ShowWorldPosition)
+            {
+                Vector3 worldPosition = mainCamera.ScreenToWorldPoint(new Vector3(clickPosition.x, clickPosition.y, mainCamera.nearClipPlane));
+                PositionText.text = "World Position: " + worldPosition.ToString();
+                Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(mainCamera, worldPosition);
+                PositionText.rectTransform.position = screenPoint;
+
+                SpawnPositionText(screenPoint);
+            }
+            else
+            {
+                PositionText.text = "Screen Position: " + clickPosition.ToString();
+                PositionText.rectTransform.position = clickPosition;
+
+                SpawnPositionText(clickPosition);
+            }
+        }
+    }
+
+    private void SpawnPositionText(Vector3 position)
+    {
+        RectTransform newText = Instantiate(PositionText.rectTransform, Canvas.transform);
+        newText.position = position;
+        newText.GetComponent<TextMeshProUGUI>().text = PositionText.text;
+    }
+}
+#endif
